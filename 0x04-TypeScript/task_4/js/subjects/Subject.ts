8d8eb4baa2e73e7f70be29495c410a478ae835e1
Peter Adjao <pa376499@gmail.com>
2025-08-21 13:47:37 +0000

    chore: Update subject interface to match  checker requirement

--- 0x04-TypeScript/task_4/js/subjects/Subject.ts
+++ 0x04-TypeScript/task_4/js/subjects/Subject.ts
@@ -5,7 +5,7 @@ namespace Subjects {
     export class Subject {
         teacher: Teacher;
 
-        setTeacher(teacher: Teacher) {
+        setTeacher(teacher:Teacher) {
             this.teacher = teacher;
         }
     }
