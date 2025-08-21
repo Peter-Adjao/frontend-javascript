f9dddd734af6f7012eca2ee9cc8189b9e98b7ada
Peter Adjao <pa376499@gmail.com>
2025-08-21 12:35:26 +0000

    task_4: add Subjects namespace with Teacher, Subject, Cpp, React, and Java classes

---
+++ 0x04-TypeScript/task_4/js/subjects/Java.ts
@@ -0,0 +1,25 @@
+/// <reference path="./Teacher.ts" />
+/// <reference path="./Subject.ts" />
+
+
+namespace Subjects {
+    export interface Teacher {
+        experienceTeachingJava?: number;
+    }
+
+    export class Java extends Subject {
+        getRequirements(): string {
+            return "Here is the list of requirements for Java";
+        }
+        
+
+        getAvailableteacher():string {
+            if (!this.teacher.experienceTeachingJava) {
+                return "No available teacher";
+            }
+            return `Available teacher: ${this.teacher.firstName}`;
+
+        }
+
+    }
+}
